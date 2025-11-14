a998bca1d29edac6a98455dc8912dc2541f1af75
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-11-14 11:44:43 +0530

    done

---
+++ methodparameter.java
@@ -0,0 +1,12 @@
+class methodparameter {
+
+    public static void add(int a, int b) {
+        int c=a+b;
+        System.out.println(c);
+        
+    }
+    public static void main(String[] args) {
+        add(10,30);
+    }
+    
+}
