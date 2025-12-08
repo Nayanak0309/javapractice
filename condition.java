58e19cfcb97989ad9d5d8150bbbb14c70adf61ca
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-12-08 23:33:35 +0530

    done

---
+++ condition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class condition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("your adult");
+        
+        }
+        else{
+            System.out.println("your not adult");
+        }
+    }
+
+    
+}
