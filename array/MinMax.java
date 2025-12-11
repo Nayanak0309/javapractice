2e510031fa4f2c7cf9346aad3981f9316336bbfc
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-12-11 23:07:31 +0530

    done

---
+++ array/MinMax.java
@@ -0,0 +1,20 @@
+package array;
+public class MinMax {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println("max no"+max);
+        System.out.println("min no"+min);
+    }
+    
+}
