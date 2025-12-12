886eb113d72dda24a8f0ab83290ba70efba4a31f
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-12-12 17:38:19 +0530

    done

---
+++ Main.java
@@ -0,0 +1,62 @@
+//Create interface Payment with pay() method. Implement in GooglePay, PhonePe, Paytm. 
+// Create interface Animal with sound() and override in Dog, Cat.
+
+//Create interface Payment with pay() method. Implement in GooglePay, PhonePe, Paytm.
+// interface Payment{
+//     void pay();
+
+// }
+// class GooglePay implements Payment{
+//     public void pay(){
+//         System.out.println("the payment throug googlepay");
+//     }
+// }
+// class PhonePe implements Payment{
+//     public void pay(){
+//         System.out.println("the payment through phonepay");
+//     }
+// }
+// class Paytm implements Payment{
+//     public void pay(){
+//         System.out.println("the payment through paytm");
+//     }
+// }
+// public class interfaces {
+//     public static void main(String[] args){
+//         Payment g = new GooglePay();
+//         g.pay();
+//         Payment ph = new PhonePe();
+//         ph.pay();
+//         Payment pai = new Paytm();
+//         pai.pay();
+
+//     }
+    
+// }
+
+
+// // Create interface Animal with sound() and override in Dog, Cat.
+
+// interface Animal1{
+//     void sound();
+// }
+// class Dog implements Animal1{
+//     public void sound(){
+//         System.out.println("bow bow");
+//     }
+// }
+// class Cat implements Animal1{
+//     public void sound(){
+//         System.out.println("meow meow");
+//     }
+// }
+// public class Main{
+//     public static void main(String[] args) {
+//         Animal1 a;
+//         a=new Dog();
+//         a.sound();
+//         a=new Cat();
+//         a.sound();
+
+//     }
+// }
